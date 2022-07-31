f40da3040ec8349059d6838ac2af225d2c2396af
chriscampaigne <campaignemc@gmail.com>
2022-07-31 13:37:38 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -6,26 +6,116 @@ const util = required('utils');
 const generatorMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [{
-    type: 'input',
-    message: 'What is the title of the project?',
-    name: 'Title',
-}, {
-    type: 'input',
-    message: 'What is the project about? Give a detailed description.',
-    name: 'Description',
-}, {
-    type: 'input',
-    message: 'Table of Contents.',
-    name: 'Table of Contents',
-}, {
-    type: 'input',
-    message: 'What does the user need to install in order to run app?',
-    name: 'Installation',
-}
+const questions = () => {
+    // using inquirer to prompt questions to user 
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false; 
+            }
+        } 
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false; 
+            }
+        }
 
-
-];
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your project name!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a description of your project!');
+                return false; 
+            }
+        }
+    }, 
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'GNU'],
+        default: ["MIT"],
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please choose a license!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the steps required to install your project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter steps required to install your project!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this app?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a usage description!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test', 
+        message: 'What command should be run to run tests?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'What does the user need to know about contributing to the repo?'
+    }
+]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
